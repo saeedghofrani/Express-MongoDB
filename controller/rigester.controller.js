0f4fe12f8bb6c532bd0f0049a528cf3db4f2ad2d
Saeed <sa.ghofraniivari@gmail.com>
2022-01-04 22:14:42 +0330

    test front,back,database validation

--- controller/rigester.controller.js
+++ controller/rigester.controller.js
@@ -2,6 +2,11 @@ const { usernameValidator, passwordValidator, firstnamedValidator, lastnameValid
 const Users = require('../models/user');
 const rigesterPage = (_request, response) => {
     response.render('rigester', { Error: '' });
+    //// const x = { username: 's', password: 's', firstName: 'sa', lastName: 's', gender: 's' };
+    //// Users.create(x, function (err, user) {
+    ////     if (err) return response.status(500).render('rigester', { Error: "Somthing went wrong in add user \n!" + err });
+    ////     response.redirect('/login');
+    //// });
 };
 const rigesterRequest = async (request, response) => {
     const { username, password, firstName, lastName, gender } = request.body;
